94cb94b331bac71d757353ff438e09e6bb288bea
fernando.teixeira <fernandoadt1@gmail.com>
2020-12-12 16:24:03 -0300

    Fixed integration tests and added kafka listener integration test

--- shipment/src/test/java/com/ecommerceapp/shipment/integration/KafkaListenerIntegrationTest.java
+++ shipment/src/test/java/com/ecommerceapp/shipment/integration/KafkaListenerIntegrationTest.java
@@ -1,58 +1,65 @@
 package com.ecommerceapp.shipment.integration;
 
 import static org.awaitility.Awaitility.await;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.ecommerceapp.inventory.model.Category;
 import com.ecommerceapp.inventory.model.Product;
-import com.ecommerceapp.shipment.model.OrderShipment;
+import com.ecommerceapp.shipment.repository.OrderShipmentRepository;
+import com.ecommerceapp.shipment.service.ShipmentService;
 import com.ecommerceapp.shipment.service.kafka.KafkaConsumerConfig;
 import com.ecommerceapp.shipment.service.kafka.MessageListenerShipment;
 import com.ecommerceapp.shop.model.Order;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith({SpringExtension.class})
-@SpringBootTest
-@EmbeddedKafka(ports = 9095, partitions = 1, controlledShutdown = true)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith({SpringExtension.class, MockitoExtension.class})
+@SpringBootTest(
+    classes = {
+      MessageListenerShipment.class,
+      OrderShipmentRepository.class,
+      ShipmentService.class,
+      MessageListenerShipment.class,
+      KafkaProducerTestConfiguration.class,
+      KafkaConsumerConfig.class
+    })
+@EmbeddedKafka(ports = 9095, partitions = 1)
 @Import({
   KafkaProducerTestConfiguration.class,
   MessageListenerShipment.class,
   KafkaConsumerConfig.class
 })
-@ActiveProfiles("test")
-public class OrderShipmentServiceIntegrationTest {
+@ContextConfiguration(classes = {KafkaConsumerConfig.class, KafkaProducerTestConfiguration.class})
+public class KafkaListenerIntegrationTest {
 
   @Autowired private EmbeddedKafkaBroker embeddedKafkaBroker;
 
   @Autowired private KafkaTemplate<String, Order> orderKafkaTemplate;
 
+  @MockBean private OrderShipmentRepository repository;
+
+  @Autowired private ShipmentService shipmentService;
+
+  @Autowired private MessageListenerShipment messageListenerShipment;
+
+  @Autowired private KafkaProducerTestConfiguration kafkaProducerTestConfiguration;
+
   @Value("${order.topic.name}")
   private String topicName;
-
-  @Autowired private MongoTemplate mongoTemplate;
-
-  public MongoTemplate getMongoTemplate() {
-    return mongoTemplate;
-  }
-
-  @AfterEach
-  void cleanUp() {
-    mongoTemplate.getDb().drop();
-  }
 
   @Test
   @DisplayName("Create Order Shipment reading message from Kafka - Success")
@@ -65,8 +72,8 @@ public class OrderShipmentServiceIntegrationTest {
     orderKafkaTemplate.send(topicName, order1);
 
     await()
-        .atMost(30, TimeUnit.SECONDS)
-        .until(() -> mongoTemplate.findAll(OrderShipment.class, "OrderShipment").size() == 1);
+        .atMost(10, TimeUnit.SECONDS)
+        .untilAsserted(() -> Mockito.verify(repository, Mockito.times(1)).save(Mockito.any()));
   }
 
   @Test
@@ -88,7 +95,7 @@ public class OrderShipmentServiceIntegrationTest {
     orderKafkaTemplate.send(topicName, order5);
 
     await()
-        .atMost(30, TimeUnit.SECONDS)
-        .until(() -> mongoTemplate.findAll(OrderShipment.class, "OrderShipment").size() == 5);
+        .atMost(10, TimeUnit.SECONDS)
+        .untilAsserted(() -> Mockito.verify(repository, Mockito.times(5)).save(Mockito.any()));
   }
 }
