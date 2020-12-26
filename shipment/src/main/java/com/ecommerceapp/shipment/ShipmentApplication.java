b98bc8a68247936de412ef1a6a89e80a530e4c2d
fernando.teixeira <fernandoadt1@gmail.com>
2020-12-26 18:17:31 -0300

    Reverted last commit

--- shipment/src/main/java/com/ecommerceapp/shipment/ShipmentApplication.java
+++ shipment/src/main/java/com/ecommerceapp/shipment/ShipmentApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
 public class ShipmentApplication {
 
   public static void main(String[] args) {
